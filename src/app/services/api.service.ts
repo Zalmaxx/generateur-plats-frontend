0d01ab1ca9257ff42939a0e9d2c65d0c324495e8
Maxime Galaxy Book <max.sevot@gmail.com>
2024-11-27 20:15:57 +0100

    .env

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import {map, Observable} from "rxjs";
 import {HttpClient, HttpResponse} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  url: string = "http://localhost:8080/version"
+  url: string = environment.apiUrl + "/api";
   constructor(private http:HttpClient) { }
 
   fonction(): Observable<string | null> {
