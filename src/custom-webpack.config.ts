0e30b8c68037f1705048520735e974e31e335f9e
Maxime Galaxy Book <max.sevot@gmail.com>
2024-11-27 22:29:39 +0100

    Update utilisation des variables d'envrionnements

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,5 +1,5 @@
 import { EnvironmentPlugin } from 'webpack';
 const Dotenv = require('dotenv-webpack');
 module.exports = {
-  plugins: [new Dotenv()],
+  plugins: [new Dotenv(), new EnvironmentPlugin({API_URL: "http://no-var-env-find.fr"})]
 };
